a7f4d36100ea1dac3a5036fdeb452cab5ee8ac1a
KatePro92 <proekaterina.92@gmail.com>
2024-04-30 11:49:05 +0400

    Updates

---
+++ precache-manifest.18aab77522f6f7ab0da7679c57e8c6ff.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec5b50c3adc81ff7eb03820dd6ba7b9",
+    "url": "/react-social_network/index.html"
+  },
+  {
+    "revision": "308b57b019e635ef2bd4",
+    "url": "/react-social_network/static/css/main.c39bedd5.chunk.css"
+  },
+  {
+    "revision": "59b49249719cc30f9143",
+    "url": "/react-social_network/static/js/2.a472d6c4.chunk.js"
+  },
+  {
+    "revision": "3a38a6ed3f9a1ea932c7",
+    "url": "/react-social_network/static/js/3.034be227.chunk.js"
+  },
+  {
+    "revision": "308b57b019e635ef2bd4",
+    "url": "/react-social_network/static/js/main.c81ce51f.chunk.js"
+  },
+  {
+    "revision": "7c890d8069e8c74e6de8",
+    "url": "/react-social_network/static/js/runtime~main.8a741db0.js"
+  },
+  {
+    "revision": "c138513e61ec7214c54af919dda6a22a",
+    "url": "/react-social_network/static/media/1688019358_sneg-top-p-standartnaya-avatarka-instagram-46.c138513e.jpg"
+  },
+  {
+    "revision": "da733e3dd717779e28f6de90f225fdc4",
+    "url": "/react-social_network/static/media/REact-Redux.da733e3d.png"
+  },
+  {
+    "revision": "eeac28e794a01334fedbfb175837cb98",
+    "url": "/react-social_network/static/media/Spinner-1s-200px.eeac28e7.svg"
+  },
+  {
+    "revision": "043ed85135ba3e79fbab08a05ca6a533",
+    "url": "/react-social_network/static/media/mylogo.043ed851.png"
+  }
+]);
